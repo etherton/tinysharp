b2876e6a8fe95b2a2e71151c32358a663a390de6
David Etherton <david.c.etherton@gmail.com>
2025-05-26 21:46:16 +0100

    switch to 6x8 font

--- tinysharp.cpp
+++ tinysharp.cpp
@@ -8,7 +8,9 @@
 
 #include "ide/editor.h"
 
-#include "font-8x8.h"
+//#include "font-4x6.h"
+#include "font-6x8.h"
+//#include "font-8x8.h"
 
 int main()
 {
@@ -17,7 +19,9 @@ int main()
     auto k = hal::keyboard::create("");
     auto v = hal::video::create("bpp=3");
 
-    v->setFont(8,8,console_font_8x8,0);
+    //v->setFont(4,6,console_font_4x6,0);
+    v->setFont(6,8,console_font_6x8,0);
+    //v->setFont(8,8,console_font_8x8,0);
 
     ide::editor e(hal::storage::create("flash"));
 
@@ -25,7 +29,7 @@ int main()
         e.newFile();
     e.draw();
     for (;;) {
-        auto ev = k->waitKeyEvent(100);
+        auto ev = k->waitKeyEvent(10);
         if (ev) {
             e.update(ev);
             e.draw();
