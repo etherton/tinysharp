c5545f8261402ba94f442f958a6c7c7c8ec933ee
David Etherton <david.c.etherton@gmail.com>
2025-05-26 11:33:39 +0100

    forgot this

--- ide/editor.h
+++ ide/editor.h
@@ -5,8 +5,19 @@ namespace ide {
 class editor {
 public:
 	editor();
-	void setFile(char *buffer,size_t length,size_t maxLength);
-	void update();
+	void setFile(char *buffer,size_t length,size_t maxLength,bool readOnly = false) {
+		m_document = buffer;
+		m_documentSize = length;
+		m_documentCapacity = maxLength;
+		m_readOnly = readOnly; // if readonly, can be in flash
+	}
+	void draw();
+
+	char *m_document;
+	size_t m_documentSize, m_documentCapacity;
+	uint32_t m_topLine;
+	uint32_t m_cursorLine, m_cursorColumn;
+	bool m_readOnly;
 };
 
 }
