21e8b5f1a53ffff9f73eb575cb58147ba294e5ea
David Etherton <david.c.etherton@gmail.com>
2025-05-27 16:40:51 -0700

    change bit depth and font at runtime

--- tinysharp.cpp
+++ tinysharp.cpp
@@ -8,20 +8,18 @@
 
 #include "ide/editor.h"
 
-//#include "font-4x6.h"
+#include "font-4x7.h"
 #include "font-6x8.h"
-//#include "font-8x8.h"
+#include "font-8x8.h"
 
 int main()
 {
     // stdio_init_all();
 
     auto k = hal::keyboard::create("");
-    auto v = hal::video::create("bpp=3");
+    hal::video::create("bpp=16");
 
-    //v->setFont(4,6,console_font_4x6,0);
-    v->setFont(6,8,console_font_6x8,0);
-    //v->setFont(8,8,console_font_8x8,0);
+    hal::video::setFont(6,8,console_font_6x8,0);
 
     ide::editor e(hal::storage::create("flash"));
 
@@ -31,7 +29,14 @@ int main()
     for (;;) {
         auto ev = k->waitKeyEvent(10);
         if (ev) {
-            e.update(ev);
+            switch (ev) {
+                case hal::modifier::LALT_BIT | hal::modifier::PRESSED_BIT | '1': hal::video::create("bpp=16"); break;
+                case hal::modifier::LALT_BIT | hal::modifier::PRESSED_BIT | '3': hal::video::create("bpp=3"); break;
+                case hal::modifier::LALT_BIT | hal::modifier::PRESSED_BIT | '4': hal::video::setFont(4,7,console_font_4x7,0); break;
+                case hal::modifier::LALT_BIT | hal::modifier::PRESSED_BIT | '6': hal::video::setFont(6,8,console_font_6x8,0); break;
+                case hal::modifier::LALT_BIT | hal::modifier::PRESSED_BIT | '8': hal::video::setFont(8,8,console_font_8x8,0); break;
+                default: e.update(ev);
+            }
             e.draw();
         }
         else
